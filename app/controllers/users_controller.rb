cc7f8b36818401e996e6f71f2263fad88df78385
Gaurav kamboj <gauravkamboj13121992@gmail.com>
2018-08-31 17:05:17 +0530

    tracker app completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,29 @@ end
     end
     render partial: 'friends/result'
   end
+
+  def add_friend
+  
+     @FriendInfo =User.find( params[:friend])
+      @createFriend=current_user.friendships.build(friend_id: params[:friend])
+    if current_user.save
+      flash[:notice] = "You have added #{@FriendInfo.first_name}  successfull as your friend"
+    else
+      flash[:danger] = "Some thing wrong happend"
+    end
+
+    redirect_to my_friends_path
+  end 
+
+  def friendship_path(friend)
+    current_user.friendships.destroy(friend)
+    flash[:danger] = "Sucessfully removed from your Friends"
+    redirect_to my_friends_path
+
+  end 
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+  end
+
 end
