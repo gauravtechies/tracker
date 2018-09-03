e8cf21304887feef013e9a4c5959dce7af5dc4bd
Gaurav kamboj <gauravkamboj13121992@gmail.com>
2018-09-03 13:08:16 +0530

    tracker_app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180829111739) do
+ActiveRecord::Schema.define(version: 20180903072904) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
@@ -48,6 +48,10 @@ ActiveRecord::Schema.define(version: 20180829111739) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
